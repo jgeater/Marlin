ea042a0ed212fdf8e90f43b308a8a090d2c8d115
James Geater <jgeater@outlook.com>
2020-10-03 15:28:51 -0400

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Ender3 SKR14Pro V2020.07.02)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Ender3 SKR14Pro V2020.10.03)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -994,7 +994,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40, -10, -1.83 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, -10, -1.86 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
